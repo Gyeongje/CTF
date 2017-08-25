9c2d89a389107d6b7b95b9f2d69a2fc80fca1837
Gyeongje <ohyt0615@gmail.com>
2017-08-25 11:52:26 +0900

    Create solve.py

---
+++ YISF 2017/MISC 150/solve.py
@@ -0,0 +1,20 @@
+data = open('C:/test/1.txt', 'rb').read()
+
+flag = []
+for i in range(20):
+    flag.append(chr(ord(data[i])^52))
+
+
+for i in range(5):
+    flag[i] = (chr(ord(flag[i])-0x10))
+
+for i in range(5, 10):
+    flag[i] = (chr(ord(flag[i])+0x20))
+
+for i in range(10, 15):
+    flag[i] = (chr(ord(flag[i])/0x2))
+
+for i in range(15, 20):
+    flag[i] = (chr(ord(flag[i])^0xaa))
+
+print ''.join(flag)
